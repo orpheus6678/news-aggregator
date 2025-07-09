3c9c0226c50f449aff69acdc0ced8092181677d0
orpheus6678 <66983935+orpheus6678@users.noreply.github.com>
2025-07-09 23:35:02 +0600

    first bold dataschema proposal

--- prisma/types.ts
+++ prisma/types.ts
@@ -1,5 +1,35 @@
+import { NewsSource } from "@prisma/client"
+
 declare global {
-  namespace PrismaJson {}
+  namespace PrismaJson {
+    type Section = {
+      name: string
+      displayName: string
+    }
+
+    type Tag = {
+      name: string
+      displayName: string
+    }
+
+    type Image = {
+      src: string
+      width: number
+      height: number
+      alt?: string
+    }
+
+    type Data = {
+      discriminator: "bdpratidin"
+      imageFooter?: string
+      signature: string
+
+      body: {
+        type: "plain"
+        text: string
+      }[]
+    }
+  }
 }
 
 export {}
